116be7c86635a6ec72e8d5e02c38613bd18f24f9
marcos-renan <devmarcos7@gmail.com>
2025-08-14 20:53:27 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def helloworld()->str:
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
